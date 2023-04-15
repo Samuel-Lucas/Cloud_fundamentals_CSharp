002c3ddb014bf04a54e7e6f0405a71f93edfbfaa
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-04-15 20:35:30 -0300

    First commit

---
+++ 3.Sqs/SqsConsumer/CustomerCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace SqsConsumer;
+
+public class CustomerCreated
+{
+    public Guid Id { get; init; }
+    
+    public string FullName { get; init; } = null!;
+
+    public string Email { get; init; } = null!;
+    
+    public string GitHubUsername { get; init; } = null!;
+
+    public DateTime DateOfBirth { get; init; }
+}
---
+++ 3.Sqs/SqsPublisher/CustomerCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace SqsPublisher;
+
+public class CustomerCreated
+{
+    public Guid Id { get; init; }
+    
+    public string FullName { get; init; } = null!;
+
+    public string Email { get; init; } = null!;
+    
+    public string GitHubUsername { get; init; } = null!;
+
+    public DateTime DateOfBirth { get; init; }
+}
