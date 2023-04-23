2ce7dc172111b2fc67e66ce95c30da6e1ec4098d
Samuel-Lucas <samuel.moreira10@outlook.com>
2023-04-23 16:40:43 -0300

    Inserindo arquivos no s3

---
+++ 6.S3/S3Playground/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+var s3Client = new AmazonS3Client();
+
+using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "samuel-aws-fundamentals",
+    Key = "files/movies.csv",
+    ContentType = "text/csv",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
